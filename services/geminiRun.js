405cbf7966b345e54b7d6a412dddd95bd0bb25e1
Ohm0025 <toyohm0025@gmail.com>
2024-08-29 14:29:12 +0700

    fix-model

--- services/geminiRun.js
+++ services/geminiRun.js
@@ -1,8 +1,13 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-require("dotenv").config();
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const api_key = process.env.API_KEY;
+
+const genAI = new GoogleGenerativeAI(api_key);
 
 const geminiRun = async (promt) => {
-  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const result = await model.generateContent(promt);
   const response = result.response;
