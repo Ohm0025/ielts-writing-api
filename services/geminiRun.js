0af42e0d697da6d89420702a19d34562843edc8b
Ohm0025 <toyohm0025@gmail.com>
2024-08-29 14:17:11 +0700

    config:service-ai

--- services/geminiRun.js
+++ services/geminiRun.js
@@ -5,8 +5,9 @@ const geminiRun = async (promt) => {
   const genAI = new GoogleGenerativeAI(process.env.API_KEY);
   const model = genAI.getGenerativeModel({ model: "gemini-pro" });
   const result = await model.generateContent(promt);
-  const response = await result.response.text();
-  return response;
+  const response = result.response;
+  const text = response.text();
+  return text;
 };
 
 module.exports = geminiRun;
