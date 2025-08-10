a31694e7e296cc3a1ec68814ea7af4e041e0a6d6
Porramat081 <porramat081@gmail.com>
2025-08-10 16:42:59 +0700

    change-api

--- services/geminiRun.js
+++ services/geminiRun.js
@@ -1,15 +1,17 @@
-const { GoogleGenerativeAI } = require("@google/generative-ai");
+const { GoogleGenAI } = require("@google/genai");
 const dotenv = require("dotenv");
 
 dotenv.config();
 
 const api_key = process.env.API_KEY;
 
-const genAI = new GoogleGenerativeAI(api_key);
+const genAI = new GoogleGenAI(api_key);
 
 const geminiRun = async (promt) => {
-  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
-  const result = await model.generateContent(promt);
+  const result = await ai.models.generateContent({
+    model: "gemini-2.5-flash",
+    contents: promt,
+  });
   const response = result.response;
   const text = response.text();
   return text;
